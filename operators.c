1286704e88a980cd78d66087700ebbd27d9798cf
shreyaa1025 <shreyu1320@gmail.com>
2025-10-29 13:33:00 +0530

    Implementing all  operations in operators.c

---
+++ operators.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+  int a = 3;
+  int b =6;
+   
+    // float a = 3;
+    // float b =6;
+   
+   printf("the sum of a and b is %d \n",a+b);
+   printf("the multiplication of a and b is %d \n",a*b);
+   printf("the division of a and b is %d \n",b/a);
+  printf("the remainder of a and b is %d \n",b%a);
+    return 0;
+}   
