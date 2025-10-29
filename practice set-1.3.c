5e27b85135a97f60e6b92799cae81f8e47b3b1ce
shreyaa1025 <shreyu1320@gmail.com>
2025-10-29 11:44:38 +0530

    Celsius to Fahrenheit conversion program
    Fix the formula for converting Celsius to Fahrenheit and correct the spelling of 'Fahrenheit' in the output.

---
+++ practice set-1.3.c
@@ -0,0 +1,12 @@
+//3. write a program to convert celsius to farhenhite
+#include <stdio.h>
+
+int main() {
+   int c =37;
+   int f;
+   
+   f=((9.0/5.0)*c)+32;
+   
+   printf("farhenhite is %d",f);
+    return 0;
+}   
