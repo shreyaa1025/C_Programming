b1fa78ed6711486c071251c89ed29278194342dd
shreyaa1025 <shreyu1320@gmail.com>
2025-10-29 11:57:16 +0530

    simple interest calculation program

---
+++ practice set-1.4.c
@@ -0,0 +1,11 @@
+//3. simple interest for a set of values representing principal 
+#include <stdio.h>
+
+int main() {
+   float p = 34.1;
+   int r = 8;
+   int t = 5;
+   
+   printf("the simple interest is %f",(p*r*t)/100);
+    return 0;
+}   
