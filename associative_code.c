3a4798000b7f2b9a02cf9bea0cd37466fd7f6d9c
shreyaa1025 <shreyu1320@gmail.com>
2025-11-07 12:35:16 +0530

    C program for operator precedence

---
+++ associative_code.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a=3;
+    int b=6;
+    int c =9;
+    printf("the value is %d",a*b/c+7);
+
+    return 0;
+}
