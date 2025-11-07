8156cf45edb80ca9104207afd5ffa230c12e77b2
shreyaa1025 <shreyu1320@gmail.com>
2025-11-07 12:57:52 +0530

    invalid statement

---
+++ practice set-2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int a =1; int b=a;
+  int v=3*3;
+  // char dt = '21 dec 2022';
+    printf("the value is %d\n",b);
+    printf("the value is %d\n",v);
+    // printf("the value is %c",char);
+
+    return 0;
+}
